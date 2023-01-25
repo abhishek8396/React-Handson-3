2e98ef642a8155303eea88eb5cbb5ee0c19b9704
anhishek1289@gmail.com <abhishek1289@gmail.com>
2023-01-25 15:21:07 +0530

    react3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './Component/Header.jsx';
+import Main from './Component/Main.jsx';
 
-function App() {
+
+
+function Form() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <center>
+        <Header/>
+        <Main/>
+      </center>
+    </>
+  )
 }
 
-export default App;
+export default Form;
---
+++ src/Component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Styling.css';
+
+function Header() {
+  return (
+    <div>
+      <h1>EMPLOYEE FEEDBACK FORM.</h1>
+    </div>
+  )
+}
+
+export default Header
